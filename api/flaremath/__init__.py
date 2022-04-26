7a6c270b27b3401976240eda026e32e533d6e030
jerremyng <jerrremyng@gmail.com>
2022-04-26 12:04:38 +0800

    testing api

--- api/flaremath/__init__.py
+++ api/flaremath/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from pydoc import plainpager
 
 import azure.functions as func
 
@@ -6,21 +7,12 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    plane = req.params.get('plane')
+    flare = req.params.get('flare')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+    return func.HttpResponse(
+            f"Your plane is {plane} and flare is {flare}",
+            status_code=200
         )
 
 
