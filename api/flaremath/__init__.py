899bf9ff55c49f3eb7496f809d3c73eae4f42b55
jerremyng <jerrremyng@gmail.com>
2022-04-26 13:09:46 +0800

    asd

--- api/flaremath/__init__.py
+++ api/flaremath/__init__.py
@@ -7,32 +7,33 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # plane = req.params.get('plane')
-    # flare = req.params.get('flare')
+    req_body = req.get_json()
+    plane = req_body.get('plane')
+    flare = req_body.get('flare')
 
-    # return func.HttpResponse(
-    #         f"Your plane is {plane} and flare is {flare}",
-    #         status_code=200
-    #     )
-
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        # return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-        return func.HttpResponse(
-        f"Your plane is {name} and flare is {name}",
-        status_code=200
-        )
-    else:
-        return func.HttpResponse(
-            f"{name}",
+    return func.HttpResponse(
+            f"Your plane is {plane} and flare is {flare}",
             status_code=200
         )
+
+
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
+
+    # if name:
+    #     # return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    #     return func.HttpResponse(
+    #     f"Your plane is {name} and flare is {name}",
+    #     status_code=200
+    #     )
+    # else:
+    #     return func.HttpResponse(
+    #         f"help please",
+    #         status_code=200
+    #     )
